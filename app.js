3b06ea0c3910c1f90d261e41ca1c992343f8a6a4
unknown <jacobdillow1@gmail.com>
2017-10-31 22:22:39 -0500

    First heroku update

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+app.get('/',function(req,res,next){
+  res.sendFile(__dirname+'/index.html');
+});
+server.listen(process.env.PORT || 5000, function(){
+  console.log("app listening on port");
+});
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+});
